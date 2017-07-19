27d0b1b0a0d21d575a20989f939948ab3a42b7c5
Frederick Crimins <fcrimins@gmail.com>
2017-07-19 15:52:04 -0400

    Moves Vectorizer from repr-engine into data-model so that it can be accessed by hamstoo/app/services/SearchService.

--- src/test/scala/com/hamstoo/models/RepresentationSpec.scala
+++ src/test/scala/com/hamstoo/models/RepresentationSpec.scala
@@ -13,7 +13,7 @@ class RepresentationSpec extends Specification {
 
   "Representation" should {
     "* be consistently hashable" in {
-      def rep = Representation("", Some("xyz"), None, "", "", "", "", None, 0, Long.MaxValue)
+      def rep = Representation("", Some("xyz"), None, "", "", "", "", Map.empty[String, Vec], 0, Long.MaxValue)
       val (a, b) = (rep, rep)
       a.hashCode mustEqual b.hashCode
       a mustEqual b
