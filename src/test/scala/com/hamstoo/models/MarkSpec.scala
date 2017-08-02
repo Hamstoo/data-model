574c8f8c2eccd7b43c7669100bdb275dfcdeebda
Frederick Crimins <fcrimins@gmail.com>
2017-08-02 16:50:51 -0400

    Forgot to update tests.

--- src/test/scala/com/hamstoo/models/MarkSpec.scala
+++ src/test/scala/com/hamstoo/models/MarkSpec.scala
@@ -12,7 +12,7 @@ class MarkSpec extends Specification {
   "Mark" should {
     "* be consistently hashable, regardless of its `score`" in {
       val uuid = UUID.randomUUID
-      val a = Mark.apply(uuid, MarkData("a subject", None, None, None, None), None)
+      val a = Mark(uuid, MarkData("a subject", None, None, None, None, None), None)
       val b = a.copy(score = Some(3.4))
       a.hashCode mustEqual b.hashCode
       a mustEqual b
