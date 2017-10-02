ed8f123d6460a38c1c1733825b08a86cff780476
fayaz <fayaz.sanaulla@gmail.com>
2017-10-02 11:13:19 +0200

    refactoring search stat spec

--- src/test/scala/com/hamstoo/models/SearchStatsSpec.scala
+++ src/test/scala/com/hamstoo/models/SearchStatsSpec.scala
@@ -4,13 +4,27 @@ import com.hamstoo.models.SearchStats.{ResultStats, Stat}
 import com.hamstoo.utils.FlatSpecWithMatchers
 
 class SearchStatsSpec extends FlatSpecWithMatchers {
-  "SearchStats" should "be incrementable with full page view events" in {
-    val query = "some query"
-    val id = "someId"
-    val weight = 22.11
-    val index = 3
-    val url = "/some/url"
-    val searchStats1 = SearchStats(query).incFpv(Some(url), id, weight, index)
+
+  val query = "some query"
+
+  val id = "someId"
+  val id2 = "anotherId"
+
+  val weight = 22.11
+  val weight2 = 11.22
+
+  val index = 3
+  val index2 = 2
+
+  val url = "/some/url"
+  val url2 = "/another/url"
+
+  lazy val searchStats1: SearchStats = SearchStats(query).incFpv(Some(url), id, weight, index)
+  lazy val searchStats2: SearchStats = searchStats1.incFpv(None, id, weight, index)
+  lazy val searchStats3: SearchStats = searchStats2.incFpv(Some(url2), id2, weight2, index2)
+
+  "SearchStats" should "be incrementable with full page view event with some url" in {
+
     searchStats1 shouldEqual SearchStats(
       query,
       marksMap = IndexedSeq(ResultStats(
@@ -23,7 +37,9 @@ class SearchStatsSpec extends FlatSpecWithMatchers {
         fpvClicksTotal = Some(1),
         weightsMap = Seq(Stat[Double](weight, fpvClicks = Some(1))),
         indexesMap = Seq(Stat[Int](index, fpvClicks = Some(1))))))
-    val searchStats2 = searchStats1.incFpv(None, id, weight, index)
+  }
+
+  it should "be incrementable with full page view event without url" in {
     searchStats2 shouldEqual SearchStats(
       query,
       marksMap = IndexedSeq(ResultStats(
@@ -36,11 +52,9 @@ class SearchStatsSpec extends FlatSpecWithMatchers {
         fpvClicksTotal = Some(1),
         weightsMap = Seq(Stat[Double](weight, fpvClicks = Some(1))),
         indexesMap = Seq(Stat[Int](index, fpvClicks = Some(1))))))
-    val id2 = "anotherId"
-    val weight2 = 11.22
-    val index2 = 2
-    val url2 = "/another/url"
-    val searchStats3 = searchStats2.incFpv(Some(url2), id2, weight2, index2)
+  }
+
+  it should "be incrementable with full page view event with other url" in {
     searchStats3 shouldEqual SearchStats(
       query,
       marksMap = IndexedSeq(
