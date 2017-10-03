c5ca8946553dc338c3f5ed4befa442acd14386ad
fayaz <fayaz.sanaulla@gmail.com>
2017-10-03 11:36:08 +0200

    refactoring search stat spec

---
+++ src/main/scala/com/hamstoo/models/HasJsonPreview.scala
@@ -0,0 +1,20 @@
+package com.hamstoo.models
+
+import play.api.libs.json.JsObject
+
+/**
+  * A trait define functionality that's required by the frontend.
+  */
+trait HasJsonPreview {
+
+  /**
+    * @return - Json object that contain data object preview information,
+    *           based on template described below
+    *             {
+    *               "id": "String based identifier"
+    *               "preview": "String or another Json object"
+    *               "type": "For example `comment` or `highlight`"
+    *             }
+    */
+  def jsonPreview: JsObject
+}
