8f565c7a787bdc7c99be8114a78a4d371ee60a82
Alex <alexshcherb84@gmail.com>
2017-10-15 19:41:44 +0300

    tika instance expanded with method getTitle, MediaTypeSupport moved to data-model

--- src/main/scala/com/hamstoo/services/TikaInstance.scala
+++ src/main/scala/com/hamstoo/services/TikaInstance.scala
@@ -1,8 +1,62 @@
 package com.hamstoo.services
 
+import java.io.{ByteArrayInputStream, InputStream}
+import javax.activation.MimeType
+
+import com.hamstoo.utils.MediaTypeSupport
 import org.apache.tika.Tika
+import org.apache.tika.metadata.{Metadata, PDF, Property, TikaCoreProperties}
+import org.apache.tika.parser.ocr.TesseractOCRConfig
+import org.apache.tika.parser.pdf.PDFParserConfig
+import org.apache.tika.parser.{AutoDetectParser, ParseContext, Parser}
+import org.apache.tika.sax.BodyContentHandler
 
 /**
   * Singleton object for Tika.
   */
-object TikaInstance extends Tika()
+object TikaInstance extends Tika() {
+
+  /* gets <title> meta tag value from any file
+     should accept Metadata instance of already parsed instance if caller includes one
+     to avoid duplication of instantiation of heavy Tika objects and double parsing
+     This method should accept either (titleTag + optMetadata) or (optByteArray)
+   */
+  def getTitle(
+      titleProperty: Option[Property] = Option.empty[Property],
+      optMetadata: Option[Metadata] = Option.empty[Metadata],
+      optArrayBytes: Option[Array[Byte]] = Option.empty[Array[Byte]]): Option[String] = {
+
+    // check if metadata is ready to extract else parse arrayByte
+    optMetadata.fold(parseTitleMetaTag(optArrayBytes))(metadata => Option(metadata.get(titleProperty.get)))
+  }
+
+  /* parse ArrayByte if Metadata instance is not accessible from caller place
+       this title metatag can also be empty so return None
+    */
+  def parseTitleMetaTag(otpByteArray : Option[Array[Byte]]): Option[String] = {
+    otpByteArray.flatMap { byteArray =>
+
+      val (parser, contentHandler, metadata, parseContext) = createCommonParameters
+
+      parseContext.set(classOf[Parser], parser)
+      val is: InputStream = new ByteArrayInputStream(byteArray)
+      parser.parse(is, contentHandler, metadata, parseContext)
+      val mimeType = new MimeType(detect(byteArray))
+      val titleProperty = if (MediaTypeSupport.isPDF(mimeType)) {
+        PDF.DOC_INFO_TITLE
+      } else {
+        TikaCoreProperties.TITLE
+      }
+      Option(metadata.get(titleProperty))
+    }
+  }
+
+  // creates common parameters for Tika Parser constructor
+  def createCommonParameters = {
+    (new AutoDetectParser(),
+    new BodyContentHandler(-1),
+    new Metadata(),
+    new ParseContext())
+  }
+
+}
--- src/test/scala/com/hamstoo/services/TikaInstanceTests.scala
+++ src/test/scala/com/hamstoo/services/TikaInstanceTests.scala
@@ -1,9 +1,18 @@
 package com.hamstoo.services
 
+import java.net.URL
 import java.nio.file.{Files, Path, Paths}
+import javax.activation.MimeType
 
 import com.hamstoo.models.Page
-import com.hamstoo.utils.TestHelper
+import com.hamstoo.services.TikaInstance.createCommonParameters
+import com.hamstoo.utils.{MediaTypeSupport, TestHelper}
+import org.apache.commons.lang3.StringEscapeUtils.escapeHtml4
+import org.apache.tika.io.IOUtils
+import org.apache.tika.metadata.{PDF, TikaCoreProperties}
+import org.apache.tika.parser.Parser
+import org.apache.tika.parser.ocr.TesseractOCRConfig
+import org.apache.tika.parser.pdf.PDFParserConfig
 
 /**
   * TikaInstanceTests
@@ -14,6 +23,10 @@ class TikaInstanceTests extends TestHelper {
   val currentFile: Path = Paths.get(pwd + "/src/test/scala/com/hamstoo/services/" + getClass.getSimpleName + ".scala")
   val content: Array[Byte] = Files.readAllBytes(currentFile)
 
+  val urlPDF = "http://www.softwareresearch.net/fileadmin/src/docs/teaching/SS13/ST/ActorsInScala.pdf"
+
+  val urlDOC =  "http://download.microsoft.com/download/f/7/3/f7395ee6-5642-4ab9-a881-786d0350e88d/skills_development_white_paper_2009.doc"
+
   "TikaInstance" should "detect MIME types" in {
     TikaInstance.detect(content) shouldEqual "text/plain"
   }
@@ -21,5 +34,30 @@ class TikaInstanceTests extends TestHelper {
   it should "detect MIME types via Page.apply" in {
     val page = Page(content)
     page.mimeType shouldEqual "text/plain"
+  }
+
+  it should "getTitle from parameters set 1" in {
+    val is = new URL(urlPDF).openStream()
+    val byteArray = Option(IOUtils.toByteArray(is))
+    val title = TikaInstance.getTitle(optArrayBytes = byteArray).getOrElse("")
+   title shouldEqual "Actors in Scala"
+  }
+
+  it should "getTitle from parameters set 2" in {
+    val is = new URL(urlDOC).openStream()
+
+    val mimeType =  new MimeType(TikaInstance.detect(content))
+    val (parser, contentHandler, metadata, parseContext) = createCommonParameters
+    val (titleKey, keywordsKey) = if (MediaTypeSupport.isPDF(mimeType)) {
+
+      (PDF.DOC_INFO_TITLE, PDF.DOC_INFO_KEY_WORDS)
+    } else {
+      parseContext.set(classOf[Parser], parser)
+      (TikaCoreProperties.TITLE, TikaCoreProperties.KEYWORDS)
+    }
+
+    parser.parse(is, contentHandler, metadata, parseContext)
+    val title = TikaInstance.getTitle(Option(titleKey), Option(metadata)).getOrElse("")
+    title  shouldEqual "- What is Nqual"
   }
 }
