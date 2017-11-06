613b84c281776f24446f55a7633d8a162251c0da
Alex <alexshcherb84@gmail.com>
2017-11-07 01:08:07 +0200

    OCR on/off controlled logics added

--- src/main/scala/com/hamstoo/services/TikaInstance.scala
+++ src/main/scala/com/hamstoo/services/TikaInstance.scala
@@ -2,6 +2,8 @@ package com.hamstoo.services
 
 import org.apache.tika.Tika
 import org.apache.tika.metadata.Metadata
+import org.apache.tika.parser.ocr.TesseractOCRConfig
+import org.apache.tika.parser.pdf.PDFParserConfig
 import org.apache.tika.parser.{AutoDetectParser, ParseContext}
 import org.apache.tika.sax.BodyContentHandler
 
@@ -12,5 +14,21 @@ import org.apache.tika.sax.BodyContentHandler
 object TikaInstance extends Tika() {
 
   /** Common constructs used in more than one place. */
-  def constructCommon() = (new BodyContentHandler(-1), new Metadata(), new ParseContext(), new AutoDetectParser())
+  def constructCommon(enableOCR: Boolean = true) = (new BodyContentHandler(-1), new Metadata(), createContext(enableOCR), new AutoDetectParser())
+
+  /** Enables OCR, enabled by default*/
+  def createContext(enableOCR: Boolean): ParseContext = {
+   val parseContext = new ParseContext()
+
+    if(!enableOCR){
+      return parseContext
+    }
+
+    val config: TesseractOCRConfig = new TesseractOCRConfig()
+    val pdfConfig: PDFParserConfig = new PDFParserConfig()
+    pdfConfig.setExtractInlineImages(true)
+    parseContext.set(classOf[TesseractOCRConfig], config)
+    parseContext.set(classOf[PDFParserConfig], pdfConfig)
+    parseContext
+  }
 }
