9e44e77eaa3342f5a2fb73d899a5006f1144ad27
Frederick Crimins <fcrimins@gmail.com>
2018-01-08 16:38:51 -0500

    Complete rewrite of "search stats" functionality and data model.  Version 0.10.8.

--- src/main/scala/com/hamstoo/models/SearchStats.scala
+++ src/main/scala/com/hamstoo/models/SearchStats.scala
@@ -1,117 +1,64 @@
 package com.hamstoo.models
 
-import com.github.dwickern.macros.NameOf._
-import com.hamstoo.models.SearchStats.{MapEl, ResultStats, Stat}
-import reactivemongo.bson.{BSONDocumentHandler, Macros}
+import java.util.UUID
 
-import scala.collection.GenSeqLike
-import scala.collection.generic.CanBuildFrom
-import scala.language.higherKinds
+import com.github.dwickern.macros.NameOf._
+import com.hamstoo.models.SearchStats.{Click, FacetVal}
+import com.hamstoo.utils.{ObjectId, TIME_NOW, TimeStamp, generateDbId}
+import reactivemongo.bson.{BSONDocumentHandler, Macros}
 
 /**
   * Database entry model for recording search results popularity respective to their search queries. The 'map'
   * fields use list types for better translation to JSON.
   *
-  * @param query    search query string for which statistics are recorded
-  * @param marksMap a map with mark ids as keys and event counts as values
-  * @param urlsMap  a map with urls as keys and event counts as values
+  * @param userId  User who initiated the search.
+  * @param markId  The mark that was clicked.
+  * @param query   The search query string for which statistics are recorded.
+  * @param facets  If any search facets were used to tailor the query (e.g. label:books).
+  * @param clicks  The "stats"; the things the user clicked as a result the search.
   */
-case class SearchStats(
-                        query: String,
-                        marksMap: IndexedSeq[ResultStats[String]] = IndexedSeq.empty[ResultStats[String]],
-                        urlsMap: IndexedSeq[ResultStats[String]] = IndexedSeq.empty[ResultStats[String]]) {
+case class SearchStats(id: ObjectId = generateDbId(Mark.ID_LENGTH),
+                       userId: UUID,
+                       markId: ObjectId,
+                       query: String,
+                       facets: Set[FacetVal] = Set.empty[FacetVal],
+                       clicks: Seq[Click] = Seq.empty[Click])
 
-  /* I believe Monocle library has all this case class update stuff implemented, but it was quicker to implement it
-  myself with a bit of type-fu than to get to know Monocle. */
-  /** Implements element update or else insert logic for lists. */
-  private def mapUpdate[T, E <: MapEl[T], S[E] <: GenSeqLike[E, S[E]]](seq: S[E], key: T, defaultElement: => E)
-                                                                      (f: E => E)
-                                                                      (implicit cbf: CanBuildFrom[S[_], E, S[E]]): S[E] =
-    seq.indexWhere(_.key == key) match {
-      case -1 => f(defaultElement) +: seq
-      case x => seq.updated(x, f(seq(x)))
-    }
+/**
+  * Extending BSONHandlers allows UUID to be converted properly for Reactive Mongo.  Otherwise one of the following
+  * errors can occur:
+  *
+  *   MongoSearchStatsDao.scala:41:22: type mismatch;
+  *   [error]  found   : (String, java.util.UUID)
+  *   [error]  required: reactivemongo.bson.Producer[reactivemongo.bson.BSONElement]
+  *
+  *   SearchStats.scala:50:85: Implicit not found 'userId': reactivemongo.bson.BSONReader[_, java.util.UUID]
+  */
+object SearchStats extends BSONHandlers {
 
-  /** Produces incremented statistics with one more link click event. */
-  def incUrl(url: String, id: String, weight: Double, index: Int): SearchStats = {
-
-    //TODO: why not to use simple parameter definition, that will be look more naturally?
-    /** increase url clicks by 1, if defined, otherwise return default value Some(1) */
-    def statInc[T]: (Stat[T]) => Stat[T] = e2 => e2.copy(urlClicks = e2.urlClicks.map(increaseByOne) orElse Some(1))
-
-    //TODO: and here
-    val statsInc: (ResultStats[String]) => ResultStats[String] = e1 => e1.copy(
-      urlClicksTotal = e1.urlClicksTotal.map(increaseByOne) orElse Some(1),
-      weightsMap = mapUpdate[Double, Stat[Double], Seq](e1.weightsMap, weight, Stat(weight))(statInc),
-      indexesMap = mapUpdate[Int, Stat[Int], Seq](e1.indexesMap, index, Stat(index))(statInc))
-
-    copy(
-      marksMap = mapUpdate[String, ResultStats[String], IndexedSeq](marksMap, id, ResultStats(id))(statsInc),
-      urlsMap = mapUpdate[String, ResultStats[String], IndexedSeq](urlsMap, url, ResultStats(url))(statsInc)
-    )
-  }
-
-  /** Produces incremented statistics with one more full page view event. */
-  def incFpv(url: Option[String], id: String, weight: Double, index: Int): SearchStats = {
-    def statInc[T]: (Stat[T]) => Stat[T] = e2 => e2.copy(fpvClicks = e2.fpvClicks.map(increaseByOne) orElse Some(1))
-
-    val statsInc: (ResultStats[String]) => ResultStats[String] = e1 => e1.copy(
-      fpvClicksTotal = e1.fpvClicksTotal.map(increaseByOne) orElse Some(1),
-      weightsMap = mapUpdate[Double, Stat[Double], Seq](e1.weightsMap, weight, Stat(weight))(statInc),
-      indexesMap = mapUpdate[Int, Stat[Int], Seq](e1.indexesMap, index, Stat(index))(statInc))
-
-    copy(
-      marksMap = mapUpdate[String, ResultStats[String], IndexedSeq](marksMap, id, ResultStats(id))(statsInc),
-      urlsMap = url map { url =>
-        mapUpdate[String, ResultStats[String], IndexedSeq](urlsMap, url, ResultStats(url))(statsInc)
-      } getOrElse urlsMap)
-  }
-
-  /** simply helper function*/
-  def increaseByOne(i: Int): Int = i + 1
-}
-
-object SearchStats {
-
-  trait MapEl[T] {
-    val key: T
-  }
+  type ClickType = Int
+  val URL_CLICK: ClickType = 0
+  val FPV_CLICK: ClickType = 1
 
   /**
-    * @param key            either mark id or url, depending on which map it is
-    * @param urlClicksTotal total count of mark link clicks in search results list
-    * @param fpvClicksTotal total count of mark full page view visits from search results list
-    * @param weightsMap     a map of mark weights in search results to mark link clicks and mark full page view visits
-    * @param indexesMap     a map of mark rankings in search results considering sorting by UI to mark link clicks and
-    *                       mark full page view visits
+    * Each click on a mark from the My Marks page will generate one of these.
+    * @param clickType  If the mark's URL is clicked this will be 0.  If it's FPV is clicked, this will be 1.
+    * @param relevance  The relevance to the search terms.  This is the value used to order the search results.
+    * @param index      The index of this mark in the results.
+    * @param ts         Timestamp of when the click occurred.
     */
-  case class ResultStats[T](
-                             key: T,
-                             urlClicksTotal: Option[Int] = None,
-                             fpvClicksTotal: Option[Int] = None,
-                             weightsMap: Seq[Stat[Double]] = Seq.empty[Stat[Double]],
-                             indexesMap: Seq[Stat[Int]] = Seq.empty[Stat[Int]]) extends MapEl[T]
+  case class Click(clickType: ClickType, relevance: Double, index: Int, ts: TimeStamp = TIME_NOW)
 
   /**
-    * @param key       either mark weight in search results or position in search results list
-    * @param urlClicks count of link clicks with this key
-    * @param fpvClicks count of full page view visits with this key
+    * Really just a key-value pair to make facet storage as flexible as possible.
     */
-  case class Stat[T](key: T, urlClicks: Option[Int] = None, fpvClicks: Option[Int] = None) extends MapEl[T]
+  case class FacetVal(name: String, value: Double)
 
+  val ID: String = com.hamstoo.models.Mark.ID;  assert(nameOf[SearchStats](_.id) == ID)
+  val USR: String = com.hamstoo.models.Mark.USR;  assert(nameOf[SearchStats](_.userId) == USR)
+  val MARKID: String = nameOf[SearchStats](_.markId)
   val QUERY: String = nameOf[SearchStats](_.query)
-  val MRKSMAP: String = nameOf[SearchStats](_.marksMap)
-  val URLSMAP: String = nameOf[SearchStats](_.urlsMap)
-  val MAPKEY: String = nameOf[ResultStats[String]](_.key)
-  val VISURL: String = nameOf[ResultStats[String]](_.urlClicksTotal)
-  val VISFPV: String = nameOf[ResultStats[String]](_.fpvClicksTotal)
-  val WGHTS: String = nameOf[ResultStats[String]](_.weightsMap)
-  val INDXS: String = nameOf[ResultStats[String]](_.indexesMap)
-  val STKEY: String = nameOf[Stat[_]](_.key)
-  val STURL: String = nameOf[Stat[_]](_.urlClicks)
-  val STFPV: String = nameOf[Stat[_]](_.fpvClicks)
-  implicit val dblStatHandler: BSONDocumentHandler[Stat[Double]] = Macros.handler[Stat[Double]]
-  implicit val intStatHandler: BSONDocumentHandler[Stat[Int]] = Macros.handler[Stat[Int]]
-  implicit val resultStatsHandler: BSONDocumentHandler[ResultStats[String]] = Macros.handler[ResultStats[String]]
+  implicit val clickHandler: BSONDocumentHandler[Click] = Macros.handler[Click]
+  implicit val facetValHandler: BSONDocumentHandler[FacetVal] = Macros.handler[FacetVal]
   implicit val searchStatsHandler: BSONDocumentHandler[SearchStats] = Macros.handler[SearchStats]
 }
