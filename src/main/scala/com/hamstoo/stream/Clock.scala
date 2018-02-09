2087814aec86c53119cc6b028b6261c299bd2e55
Frederick Crimins <fcrimins@gmail.com>
2018-02-09 15:38:31 -0500

    84: Starting to build clock sink and basing graphs on BidiFlows (last commit before vaca).

---
+++ src/main/scala/com/hamstoo/stream/Clock.scala
@@ -0,0 +1,7 @@
+package com.hamstoo.stream
+
+import akka.stream.scaladsl.Sink
+
+class Clock extends Sink {
+
+}
--- src/main/scala/com/hamstoo/stream/DataStream.scala
+++ src/main/scala/com/hamstoo/stream/DataStream.scala
@@ -6,9 +6,8 @@ import play.api.Logger
 
 /**
   * A wrapper around an Akka Stream.
-  * @tparam T
   */
-trait DataStream[T] {
+abstract class DataStream[T](implicit clock: Clock) {
 
   val logger: Logger = Logger(classOf[DataStream[T]])
 
@@ -18,8 +17,9 @@ trait DataStream[T] {
 }
 
 /**
-  *
-  * @param source
-  * @tparam T
+  * A DataStream that is composed of a source provided by elsewhere.  For example, a stream that computes
+  * a function of one or more other streams may, as its last step, wrap up its resulting stream in a DataStreamRef.
+  * A more proper name perhaps would be DataStreamFunction.
+  * @param source  Outside provided Akka Source
   */
-case class DataStreamRef[T](override val source: Source[Datum[T], NotUsed]) extends DataStream[T]
+case class DataStreamRef[T](override val source: Source[Datum[T], NotUsed])(implicit clock: Clock) extends DataStream[T]
