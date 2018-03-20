e818d0414e501c95f307a1e555d407c6ce364782
Frederick Crimins <fcrimins@gmail.com>
2018-03-20 14:15:19 -0400

    84: Finally got MergeHub working in FacetsModel.run by...not using MergeHub; using simpler Merge instead.

--- src/main/scala/com/hamstoo/stream/FacetsModel.scala
+++ src/main/scala/com/hamstoo/stream/FacetsModel.scala
@@ -1,8 +1,8 @@
 package com.hamstoo.stream
 
 import akka.NotUsed
-import akka.stream.Materializer
-import akka.stream.scaladsl.{Keep, MergeHub, Sink}
+import akka.stream.{Materializer, SourceShape}
+import akka.stream.scaladsl.{GraphDSL, Merge, Sink, Source}
 import com.google.inject.{Inject, Injector}
 import play.api.Logger
 
@@ -20,14 +20,16 @@ class FacetsModel @Inject()(injector: Injector)
 
   val logger: Logger = Logger(classOf[FacetsModel])
 
-  // set of all facets to be computed by this model
+  // a set of all the facets/statistics/metrics to be computed by this model
   protected val facets = mutable.Map.empty[String, DataStream[Double]]
 
   /** Add a facet to be computed by this model. */
-  def add[T <:DataStream[Double] :TypeTag :ClassTag](/*name: String*/): Unit = {
+  def add[T <:DataStream[Double] :TypeTag :ClassTag](mbName: Option[String] = None): Unit = {
 
-    val name: String = typeTag[T].tpe.erasure.toString
+    val name: String = mbName.getOrElse(classTag[T].runtimeClass.getSimpleName)
     logger.info(s"Adding data stream: $name")
+    if (facets.contains(name))
+      throw new Exception(s"Duplicate '$name' named facets detected")
 
     // note that scalaguice still uses old Scala version implicit Manifests (presumably for backwards compatibility)
     import net.codingwell.scalaguice.InjectorExtensions._
@@ -38,16 +40,55 @@ class FacetsModel @Inject()(injector: Injector)
     facets += name -> ds
   }
 
-  /** Run the entire model, including starting the clock. */
-  def run(sink: Sink[Data[Double], Future[Seq[Data[Double]]]]): Future[Seq[Data[Double]]] = {
+  /**
+    * Run the entire model, including starting the clock.
+    * @param sink  The sink to use to run the source.
+    * @tparam T  Depends on the type of sink passed in.  `Sink.seq` would mean T is a `Seq[Data[Double]]` while
+    *            `Sink.fold[Double, Data[Double]]` would mean T is a lonely `Double`.
+    * @return  Returns a future containing the result of the sink.
+    */
+  def run[T](sink: Sink[Data[Double], Future[T]]): Future[T] = {
 
-    // this properly gets the results into a collection when the sink is a Sink.seq
-    val result = facets.head._2.source.runWith(sink)
+    // simultaneously allow the facets to start producing data (it's safer to start the clock after materialization
+    // because of weird buffer effects and such)
+    val result = source.runWith(sink)
+    clock.start()
+
+    result
+  }
+
+  /** Wire up the model by merging all the individual facets, but don't run it, and don't start the clock. */
+  def source: Source[Data[Double], NotUsed] = Source.fromGraph(GraphDSL.create() { implicit b =>
+    import GraphDSL.Implicits._
+
+    val merge = b.add(Merge[Data[Double]](facets.size))
+
+    // wire all of the facets into the hub
+    facets.zipWithIndex.foreach { case ((name, ds), i) =>
+
+      // label the source with its facet name so that we can tell them apart on the other side
+      val labeledSource = ds.source.map { data =>
+        data.copy(values = data.values.map { case (eid, value) =>
+          EntityId.merge(FacetName(name), eid) -> value
+        })
+      }.named(name) // unsure if this actually has any effect
+
+      labeledSource ~> merge.in(i)
+    }
+
+    SourceShape(merge.out)
+  })
+
+  /**
+    * No need for this to use a MergeHub (see Merge implementation above), which I couldn't get
+    * working anyway because the MergeHub's `result` Future never seems to complete.
+    */
+  /*def run[T](sink: Sink[Data[Double], Future[T]]): Future[T] = {
 
     // materialize the MergeHub so that we can dynamically wire facets into it
     // see here https://github.com/akka/akka/issues/21693 for how I figured out the `toMat(sink)(Keep.both)` part
-    /*val ppbs = DataStream.DEFAULT_BUFFER_SIZE
-    val (hubSink, result): (Sink[Data[Double], NotUsed], Future[Seq[Data[Double]]]) =
+    val ppbs = DataStream.DEFAULT_BUFFER_SIZE
+    val (hubSink, result): (Sink[Data[Double], NotUsed], Future[T]) =
       MergeHub.source[Data[Double]](perProducerBufferSize = ppbs).toMat(sink)(Keep.both).run()
 
     // wire all of the facets into the hub
@@ -58,14 +99,12 @@ class FacetsModel @Inject()(injector: Injector)
         data.copy(values = data.values.map { case (eid, value) =>
           EntityId.merge(FacetName(name), eid) -> value
         })
-      }
+      }.named(name) // unsure if this actually has any effect
 
-      labeledSource.named(name).runWith(hubSink)
-    }*/
+      labeledSource.runWith(hubSink)
+    }
 
-    // simultaneously allow the facets to start producing data
     clock.start()
-
     result
-  }
+  }*/
 }
