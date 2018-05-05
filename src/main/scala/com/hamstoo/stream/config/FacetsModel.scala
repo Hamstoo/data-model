78f0612ce7c085af7f615359f35b4cd2cea821fa
Frederick Crimins <fcrimins@gmail.com>
2018-05-05 07:15:05 -0400

    #329: Minor bugfix to last commit.

--- src/main/scala/com/hamstoo/stream/config/FacetsModel.scala
+++ src/main/scala/com/hamstoo/stream/config/FacetsModel.scala
@@ -26,7 +26,7 @@ class FacetsModel @Inject()(injector: Injector)
 
   import FacetsModel._
   val logger: Logger = Logger(classOf[FacetsModel])
-  logger.info(s"Constructing model: ${classOf[FacetsModel].getName}")
+  logger.debug(s"Constructing model: ${classOf[FacetsModel].getName}")
 
   // a set of all the facets/statistics/metrics to be computed by this model
   protected val facets = mutable.Map.empty[String, DataStream[_]]
