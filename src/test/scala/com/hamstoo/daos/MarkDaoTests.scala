5455ab99d2848b094963b3c1ed509ab6fbdb8385
fsanaulla <fayaz.sanaulla@gmail.com>
2018-06-03 16:01:25 +0300

    issue-267: post-merge fixes

--- src/test/scala/com/hamstoo/daos/MarkDaoTests.scala
+++ src/test/scala/com/hamstoo/daos/MarkDaoTests.scala
@@ -236,18 +236,4 @@ class MarkDaoTests
     masked.mark.tags.get should contain("updateMarkRefTAG")
     masked.mark.rating.get shouldEqual 3.0
   }
-
-  it should "(UNIT) retrieve by userId and URL 2" in {
-    val url1 = "https://mail.google.com/mail/u/0/#inbox/162e903e20ce4af3"
-    val url2 = "https://mail.google.com/mail/u/0/#inbox/162e5aa71f6ebdff"
-
-    val m1 = Mark(uuid1, "markID1", mark = MarkData("subj1", Some(url1)))
-    val m2 = Mark(uuid1, "markID2", mark = MarkData("subj2", Some(url2)))
-
-    marksDao.insert(m1).futureValue shouldEqual m1
-    marksDao.retrieveByUrl(url1, uuid1).futureValue.value shouldEqual m1
-
-    marksDao.insert(m2).futureValue shouldEqual m2
-    marksDao.retrieveByUrl(url2, uuid1).futureValue.value shouldEqual m2
-  }
 }
