85cdb46982ef9cbbc139437b58eaea1e87f5904c
Frederick Crimins <fcrimins@gmail.com>
2018-06-11 15:56:16 -0400

    #319: Restrict streamed data to between clock start and end (for profile dots enhancement).

--- src/main/scala/com/hamstoo/stream/Clock.scala
+++ src/main/scala/com/hamstoo/stream/Clock.scala
@@ -8,22 +8,18 @@ import akka.stream.Materializer
 import akka.stream.scaladsl.Source
 import com.google.inject.name.Named
 import com.google.inject.Inject
-import com.hamstoo.stream.Tick.Tick
 import com.hamstoo.utils.{ExtendedDurationMils, ExtendedTimeStamp, TimeStamp}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Promise}
 
 /**
-  * A mocked clock implemented as an Akka Source.
-  *
-  * TODO: make interval private so that users cannot increment/decrement on their own but must instead use
-  * TODO:   increment/decrement-named methods (to handle irregular intervals involving weekends and months and such)
+  * A mocked clock, implemented as an Akka Source.
   */
 @com.google.inject.Singleton
 case class Clock @Inject()(@Named(ClockBegin.name) begin: ClockBegin.typ,
                            @Named(ClockEnd.name) end: ClockEnd.typ,
-                           @Named(ClockInterval.name) interval: ClockInterval.typ)
+                           @Named(ClockInterval.name) private val interval: ClockInterval.typ)
                           (implicit mat: Materializer)
 
     extends ElemStream[Datum[TimeStamp]](bufferSize = 1) {
@@ -61,7 +57,7 @@ case class Clock @Inject()(@Named(ClockBegin.name) begin: ClockBegin.typ,
 
         // inclusive end: shouldn't hurt anything and could help (conservative), but note that we could skip
         // over end, making this point moot, if `end - start` is not an exact number of `interval`s
-        val bool = currentTime <= end
+        val bool = currentTime < end
         if (!bool) logger.info(s"\033[33mClock complete\033[0m")
         bool
       }
@@ -72,11 +68,11 @@ case class Clock @Inject()(@Named(ClockBegin.name) begin: ClockBegin.typ,
         // wait for `started` to be true before ticks start incrementing
         Await.result(started.future, Duration.Inf)
 
-        // TODO: would it ever make sense to have a clock Datum's knownTime be different from its sourceTime or val?
+        // would it ever make sense to have a clock Datum's knownTime be different from its sourceTime or val?
+        val previousTime = currentTime
+        currentTime = math.min(currentTime + interval, end) // ensure we don't go beyond `end`
         logger.debug(s"\033[33mTICK: ${currentTime.tfmt}\033[0m")
-        val r = currentTime
-        currentTime += interval
-        Tick(r)
+        Tick(currentTime, previousTime)
       }
     }
   }.named("Clock")
