5aa56d1cc37f11cbe1739edbcb6fe6baf0b50452
Frederick Crimins <fcrimins@gmail.com>
2018-08-27 12:51:25 -0400

    #266: Code review and add TODOs.

--- src/main/scala/com/hamstoo/daos/RecommendationDao.scala
+++ src/main/scala/com/hamstoo/daos/RecommendationDao.scala
@@ -1,3 +1,6 @@
+/*
+ * Copyright (C) 2017-2018 Hamstoo, Inc. <https://www.hamstoo.com>
+ */
 package com.hamstoo.daos
 
 import java.util.UUID
@@ -5,58 +8,83 @@ import java.util.UUID
 import com.google.inject.Inject
 import com.hamstoo.models.Recommendation
 import javax.inject.Singleton
+
 import org.joda.time.DateTime
 import play.api.Logger
 import reactivemongo.api.DefaultDB
 import reactivemongo.api.collections.bson.BSONCollection
 import reactivemongo.api.indexes.{Index, IndexType}
-import reactivemongo.bson.{BSONArray, BSONDocument}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
-
-
+/**
+  * TODO 266
+  *
+  */
 @Singleton
 class RecommendationDao @Inject()(implicit db: () => Future[DefaultDB]) {
 
+  import Recommendation._
   import com.hamstoo.utils._
-  val logger: Logger = Logger(classOf[RecommendationDao])
+  val logger: Logger = Logger(getClass)
 
   // database collection
-  private def recommendationDB(): Future[BSONCollection] = db().map(_.collection("recommendations"))
+  private def dbColl(): Future[BSONCollection] = db().map(_.collection("recommendations"))
 
   // indexes for this mongo collection
-  recommendationDB().map(_.indexesManager.ensure(Index(Seq("userId" -> IndexType.Ascending), name = Some("userId_idx"))))
-  recommendationDB().map(_.indexesManager.ensure(Index(Seq("ts" -> IndexType.Descending), name = Some("ts_idx"))))
+  private val indxs: Map[String, Index] =
+    Index(USR -> IndexType.Ascending :: TIMESTAMP -> IndexType.Descending :: Nil) % s"bin-$USR-1-$TIMESTAMP-1" ::
+    Nil toMap;
+  Await.result(dbColl() map (_.indexesManager ensure indxs), 134 seconds)
 
-
-  def insertOne(recommendation: Recommendation) = for {
-    c <- recommendationDB()
+  /**
+    * TODO 266
+    * @param recommendation
+    * @return
+    */
+  def insertOne(recommendation: Recommendation): Future[Unit] = for {
+    c <- dbColl()
     _ = logger.info(s"Inserting: $recommendation")
     wr <- c.insert(recommendation)
     _ <- wr.failIfError
   } yield logger.info(s"Successfully inserted: $recommendation")
 
-  def insertMany(recommendations: Seq[Recommendation]) = {
+  /**
+    * TODO 266
+    * @param recommendations
+    * @return
+    */
+  def insertMany(recommendations: Seq[Recommendation]): Future[Unit] = {
     recommendations foreach insertOne
     Future(Unit)
   }
 
+  /**
+    * TODO 266
+    * @param userId
+    * @param nDaysBack  Only retrieve recommendations from, at most, this far back in time.
+    * @return
+    */
+  def retrieve(userId: UUID, nDaysBack: Int = RecommendationDao.DEFAULT_DAYS_BACK): Future[Seq[Recommendation]] = {
+    logger.info(s"Retrieving $nDaysBack days' recommendations for user $userId")
 
-  def retrieve(user: UUID) = {
-    logger.info(s"Retrieving last week feeds for user $user")
-    val oneWeekAgo = new DateTime() minusDays 7 getMillis
-    val mongoTimeObj = BSONDocument(
-      "ts" -> BSONDocument(
-        "$gte" -> oneWeekAgo,
-        "$lte" -> new DateTime().getMillis
-      ))
-    val q = BSONDocument("$and" -> BSONArray(mongoTimeObj, BSONDocument("userId" -> user.toString)))
+    // TODO 266: do we really only want to retrieve recommendations from up to 7 days back?  or is the purpose of the
+    // TODO 266:   7 to limit the keywords with which we're computing recentAutoGenKws ???
+
+    val oneWeekAgo = new DateTime().minusDays(nDaysBack).getMillis
+    val q = d :~ USR -> userId :~
+                 TIMESTAMP -> (d :~ "$gte" -> oneWeekAgo) :~ // TODO 266: these 2 TIMESTAMP were previously combined. was there a reason?
+                 TIMESTAMP -> (d :~ "$lte" -> new DateTime().getMillis) // TODO 266: is it even possible to have future recs?
     for {
-      c <- recommendationDB()
-//      r <- c.find(d :~ "userId" -> user.toString).sort(d :~ "ts" -> -1).coll[Recommendation, Seq]()
-      r <- c.find(q).sort(d :~ "ts" -> -1).coll[Recommendation, Seq]()
+      c <- dbColl()
+      //r <- c.find(d :~ USR -> user.toString).sort(d :~ TIMESTAMP -> -1).coll[Recommendation, Seq]()
+      r <- c.find(q).sort(d :~ TIMESTAMP -> -1).coll[Recommendation, Seq]()
     } yield r
   }
+}
+
+object RecommendationDao {
+  val DEFAULT_DAYS_BACK = 7
 }
--- src/main/scala/com/hamstoo/models/Recommendation.scala
+++ src/main/scala/com/hamstoo/models/Recommendation.scala
@@ -1,10 +1,23 @@
+/*
+ * Copyright (C) 2017-2018 Hamstoo, Inc. <https://www.hamstoo.com>
+ */
 package com.hamstoo.models
 
 import java.util.UUID
 
+import com.github.dwickern.macros.NameOf.nameOf
 import com.hamstoo.utils.{TIME_NOW, TimeStamp}
 import reactivemongo.bson.{BSONDocumentHandler, Macros}
 
+/**
+  * TODO 266
+  *
+  * @param userId
+  * @param source  One of the values from RecSourceEnum (as a String).
+  * @param params
+  * @param url
+  * @param ts
+  */
 case class Recommendation (
   userId: UUID,
   source: String,
@@ -14,5 +27,22 @@ case class Recommendation (
 )
 
 object Recommendation extends BSONHandlers {
-  implicit val RecommandeyionJson: BSONDocumentHandler[Recommendation] = Macros.handler[Recommendation]
+
+  val USR: String = Mark.USR; assert(USR == nameOf[Recommendation](_.userId))
+  val TIMESTAMP: String = UserStats.TIMESTAMP; assert(TIMESTAMP == nameOf[Recommendation](_.userId))
+
+  /**
+    * Enumeration of various sources of recommendations.
+    * See Representation.VecEnum for something similar.
+    *
+    * TODO 266: if we're comparing request URLs against these values in mediumPostsToRecommendation, then we may
+    *           need to use Value("DuckDuckGo") but that should really be made more obvious via a defined function
+    */
+  object SrcEnum extends Enumeration {
+    val DUCK_DUCK_GO,
+        MEDIUM
+      = Value
+  }
+
+  implicit val recommendationHandler: BSONDocumentHandler[Recommendation] = Macros.handler[Recommendation]
 }
