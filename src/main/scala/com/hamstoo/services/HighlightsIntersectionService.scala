95e89f145b579d554a6e54a6a2e2b3097b6e39b9
Frederick Crimins <fcrimins@gmail.com>
2018-09-19 16:10:19 -0400

    Remove HighlightsIntersectionService.isSubset/isEdgeIntrsc in favor of calling their inner functions (which have since been moved to Highlight.Position).

--- src/main/scala/com/hamstoo/services/HighlightsIntersectionService.scala
+++ src/main/scala/com/hamstoo/services/HighlightsIntersectionService.scala
@@ -34,62 +34,41 @@ class HighlightsIntersectionService @Inject()(implicit db: HighlightDao, ec: Exe
     // find a single overlapping/touching/joinable existing highlight
     mbOverlap = origHls.view.map { origHl =>
 
-                  // check for edge intersections as well as inclusion
-                  (origHl, isEdgeIntsc(origHl, hl), isSubset(origHl, hl))
+        lazy val origFirstIntersection = hl.startsWith(origHl)
+        lazy val newFirstIntersection = origHl.startsWith(hl)
 
-                }.find { case (_, edge, subs) => edge.isDefined || subs.isDefined }
+        // if the new one is a subset of an existing highlight, then there's nothing to do (although we could
+        // delete and re-insert it to confirm that all previous inserts/intersections went okay)
+        if (hl.isSubseq(origHl)) Some { () =>
+          Future.successful(origHl)
+
+        // update existing highlight if it's a subset of the new one (hl), and use origHl.id which the
+        // chrome-extension depends on
+        } else if (origHl.isSubseq(hl)) Some { () =>
+          db.delete(origHl.usrId, origHl.id).flatMap(delAt => add(hl.copy(id = origHl.id, timeFrom = delAt)))
+
+        // if neither is a complete subset of the other, update existing highlight with a union of the two
+        } else if (origFirstIntersection.nonEmpty) Some {
+          () => {
+            val u = origHl.union(hl)
+            db.delete(origHl.usrId, origHl.id).flatMap(delAt => add(u.copy(id = origHl.id, timeFrom = delAt)))
+          }
+
+        } else if (newFirstIntersection.nonEmpty) Some {
+          () => {
+            val u = hl.union(origHl)
+            db.delete(origHl.usrId, origHl.id).flatMap(delAt => add(u.copy(id = origHl.id, timeFrom = delAt)))
+          }
+
+        // just insert the new highlight if none intersect with it
+        } else None
+
+      }.find(_.isDefined).flatten // flatten the double Options
 
     _ = if (mbOverlap.isDefined) logger.debug(s"Found existing highlight ${mbOverlap.get} ... that overlaps with new highlight $hl")
 
-    newHl <- {
+    // just insert the new highlight if none intersect with it
+    newHl <- mbOverlap.fold(db.insert(hl).map(_ => hl))(_())
 
-      // just insert the new highlight if none intersect with it ...
-      mbOverlap.fold(db.insert(hl).map(_ => hl)) {
-
-        // ... otherwise delete the existing original and recurse
-        case (origHl, isEdgeWithOrigFirst, isNewSubsetOfOrig) => for {
-
-          delTime <- db.delete(origHl.usrId, origHl.id)
-          _ = logger.debug(s"Deleted original highlight ${origHl.id} (isEdgeWithOrigFirst=$isEdgeWithOrigFirst, isNewSubsetOfOrig=$isNewSubsetOfOrig)")
-
-          newHl <- {
-
-            // recurse with existing highlight, if the new one is a subset of it (recursive step should really just
-            // lead to an (re)insert, if all previous inserts/intersections went okay, but recurse anyway, just in case)
-            if (isNewSubsetOfOrig.contains(true)) add(origHl.copy(timeFrom = delTime))
-
-            // update existing highlight if it's a subset of the new one (hl), and use origHl.id which the
-            // chrome-extension depends on
-            else if (isNewSubsetOfOrig.contains(false)) add(hl.copy(id = origHl.id, timeFrom = delTime))
-
-            // if neither is a complete subset of the other, update existing highlight with a union of the two
-            else {
-              val u = if (isEdgeWithOrigFirst.contains(true)) origHl.union(hl) else hl.union(origHl)
-              add(u.copy(id = origHl.id, timeFrom = delTime)) // maintain the same id as a sanity check
-            }
-          }
-        } yield newHl
-      }
-    }
   } yield newHl // return produced, updated, or existing highlight
-
-  /** Checks whether one position is a subset of another. */
-  def isSubset(hlA: Highlight, hlB: Highlight): Option[Boolean] = {
-
-    // test if sets of paths are subsets and whether edge elements completely overlap
-    if (hlA.isSubseq(hlB)) Some(false) else if (hlB.isSubseq(hlA)) Some(true) else None
-  }
-
-  /**
-    * Checks if two highlights overlap by checking XPath lists intersection.
-    *
-    * @return  0 if no overlap
-    *          1 if    overlap with A before B
-    *         -1 if    overlap with B before A
-    */
-  def isEdgeIntsc(hlA: Highlight, hlB: Highlight): Option[Boolean] = {
-
-    // look for sequences of paths that are tails of one Position and start of another
-    if (hlB.startsWith(hlA).nonEmpty) Some(true) else if (hlA.startsWith(hlB).nonEmpty) Some(false) else None
-  }
 }
